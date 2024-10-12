e9f99006a0a126fd207816c8e84de0e2dec7ac3b
RenYixin123 <1773244945@qq.com>
2024-10-12 21:21:58 +0800

    Create crowdfunding_db.js

---
+++ sever/crowdfunding_db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: '123456', // mysql password
+        database: 'crowdfunding_db', // database name
+        port: '3306' // mysql port
+    }
+}
