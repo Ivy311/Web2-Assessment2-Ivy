3d0e56743a0d63596d0ebc2700fd687636f513fc
RenYixin123 <1773244945@qq.com>
2024-10-12 21:22:57 +0800

    Create index.js

---
+++ sever/index.js
@@ -0,0 +1,17 @@
+const Api = require('./api');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// express
+const express = require('express');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//api router prefix
+app.use('/api', Api);
+
+//listen 3000
+app.listen(3000);
+
+console.log('success listen at port:3000...');
