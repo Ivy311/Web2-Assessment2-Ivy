5435371b1c780fb55ac8d78b6e2e9c4876ec3c6e
RenYixin123 <1773244945@qq.com>
2024-10-14 00:43:09 +0800

    Update sqlMap.js

--- sever/sqlMap.js
+++ sever/sqlMap.js
@@ -11,10 +11,20 @@ var sqlMap = {
                                 FROM fundraiser f
                                 JOIN category c ON f.CATEGORY_ID = c.CATEGORY_ID
                                 WHERE (f.FUNDRAISER_id = ?)`,
-        getDonationList: `select * from donation d WHERE (d.fundraiser_id = ?)`
+        getDonationList: `select * from donation d WHERE (d.FUNDRAISER_ID = ?)`,
+        updateCurrentFunding: `UPDATE fundraiser SET CURRENT_FUNDING = CURRENT_FUNDING + ? WHERE fundraiser.FUNDRAISER_ID = ?`
     },
     insert: {
-        donation : `insert into donation (date, amount, giver, fundraiser_id) values(?,?,?,?)`
+        donation : `insert into donation (DATE, AMOUNT, GIVER, FUNDRAISER_ID) values(?,?,?,?)`
+    },
+    admin: {
+        searchFundRaiserList: `select f.*, c.NAME AS CATEGORY_NAME
+                                FROM fundraiser f
+                                JOIN category c ON f.CATEGORY_ID = c.CATEGORY_ID
+                                WHERE (f.CATEGORY_ID = ? OR ? IS NULL) AND (f.ORGANIZER = ? OR ? IS NULL) AND (f.CITY = ? OR ? IS NULL) AND (f.ACTIVE = ? OR ? IS NULL)`,
+        addFundraiser: `insert into fundraiser (CAPTION, ORGANIZER, CITY, TARGET_FUNDING, CURRENT_FUNDING, CATEGORY_ID, ACTIVE) values(?,?,?,?,?,?,?)`,
+        updateFundraiser: `UPDATE fundraiser SET CAPTION = ?, ORGANIZER = ?, CITY = ?, TARGET_FUNDING = ?, CATEGORY_ID = ?, ACTIVE = ? WHERE fundraiser.FUNDRAISER_ID = ?`,
+        deleteFundraiser: `DELETE FROM fundraiser WHERE fundraiser.FUNDRAISER_ID = ?`
     }
 }
 module.exports = sqlMap
