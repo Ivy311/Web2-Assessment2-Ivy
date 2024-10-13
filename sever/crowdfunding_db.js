5a4ee01a473f00ad2380e659930ce197801e9a94
Ivy311 <2249544573@qq.com>
2024-10-14 01:51:09 +0800

    Update crowdfunding_db.js

--- sever/crowdfunding_db.js
+++ sever/crowdfunding_db.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: '123456', // mysql password
+        password: '12345', // mysql password
         database: 'crowdfunding_db', // database name
         port: '3306' // mysql port
     }
